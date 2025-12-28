0c5ebb72bd6e2a70a84bbd54a42461ad068c230f
choi <choi@choeui-MacBookPro.local>
2025-12-29 06:58:24 +0900

    0006

--- src/main/java/com/back/global/jpa/entity/BaseEntity.java
+++ src/main/java/com/back/global/jpa/entity/BaseEntity.java
@@ -1,5 +1,6 @@
 package com.back.global.jpa.entity;
 
+import com.back.global.global.GlobalConfig;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
@@ -8,9 +9,12 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 @Getter
-// 모든 엔티티들의 조상
 public class BaseEntity {
     public String getModelTypeCode() {
         return this.getClass().getSimpleName();
+    }
+
+    protected void publishEvent(Object event) {
+        GlobalConfig.getEventPublisher().publish(event);
     }
 }
